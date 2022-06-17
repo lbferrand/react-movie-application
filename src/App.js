2d9b3029d8bde5894646247c541eee33b649294a
lbferrand <lbferrand@gmail.com>
2022-06-16 20:18:49 -0400

    Create react application & some components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//Components
+import Header from "./components/Header";
+
+//Styles
+import { GlobalStyle } from "./GlobalStyle";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+};
+
+export default App;
